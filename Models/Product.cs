5a4d21c283bcf2ff223d1ee16f1c490055f39f39
kazimbeyli <sunaykazimbeyli@gmail.com>
2025-03-28 00:51:28 +0400

    initial commit

---
+++ Models/Product.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace EncapsulationAndRecord.Models
+{
+    internal class Product
+    {
+        private string _id;
+        private string _brandname;
+        private string _model;
+        private decimal _price;
+        private decimal _cost;
+        private decimal _income;
+        private int _count;
+
+        public string Id
+        {
+            get
+            {
+                return _id;
+            }
+           
+        }
+        public string Brandname
+        {
+            get {
+                return _brandname;
+            }
+            set {
+                value=value.Trim();
+                if (value.Length>=2) 
+                {
+                _brandname = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ad formati duzgun deyil");
+                }
+            }
+        }
+
+        public string Model
+        {
+            get {
+                return _model;
+            }
+            set
+            {
+                value = value.Trim();
+                if (value.Length >= 2)
+                {
+                    _model = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ad formati duzgun deyil");
+                }
+            }
+        }
+
+        public decimal Price
+        {
+            get {
+                return _price; 
+                }
+            set 
+            {
+                if (value>0)
+                {
+                    _price = value;
+                }
+            }
+        }
+
+        public decimal Cost
+        {
+            get { 
+                return _cost;
+            }
+            set {
+                if (value>0)
+                {
+                    _cost = value;
+                }
+            }
+        }
+
+        public decimal Income
+        {
+            get { return _income ; }
+        }
+
+        public int Count
+        {
+            get { return _count; }
+            set {
+                if (value>=0)
+                {
+                    _count = value;
+                }
+
+            }
+        }
+        public Product( string brandname, string model, decimal price , decimal cost , int count)
+        {
+            _brandname=brandname;
+            _model=model;       
+            _price=price;
+            _cost=cost;
+            _count=count;
+        
+            _id = string.Concat(_brandname.Substring(0, 2), _model.Substring(0, 2));
+             
+            if (count-->0)
+            {
+                _income = 0;
+                _income += _price;
+                
+            }
+            else if (count==0)
+            {
+                _income = 0;
+            }
+
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Id :{_id} BrandName: {_brandname} Model:{_model} Price:{_price} Cost:{_cost} Income:{_income} Count:{Count}");
+        }
+        public void Sale()
+        {
+            if (_count>0)
+            {    _income = 0;
+                _income += _price - _cost;
+                _count--;
+                Console.WriteLine($"satis heyata kecirilidi Income:{_income} Count:{_count}");
+            }
+            else
+            {
+                Console.WriteLine("mehsul bitib");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using EncapsulationAndRecord.Models;
+
+namespace EncapsulationAndRecord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product("Apple","Iphone",1200.3m, 12.1m,1);
+
+            product.GetInfo();
+            product.Sale();
+        }
+    }
+}
+//Product classi yaradirsiz:
+// Id
+// BrandName,
+// Model,
+// Price,
+// Cost, - xercleri   
+// Income, - umumi gelir
+// Count - mehsulun sayi
+// fieldleri olur. Constructor ishe dushdukde Income ve Id-den bashqa butun deyerleri qebul edir.(Income 0-dan bashliyib mehsul satildiqca artacaq. Count menfi ola bilmez. Menfi gonderilerse 0 verilsin).
+//Id-ni ctor ishe dushdukde(yəni constructorun icerisinde) BrandName ilk 2 simvolunu Modelin ilk 2 simvolunu birleshdirerek teyin edirsiz. Meselen( Brand adi : Apple, Model:Iphone13 dirse Id:ApIp olmalidir )
+ 
+//GetInfo() methodu olur.
+
+//Sale() methodu olur.Eger kifayet qeder mehsul varsa Satishi heyata kecirir satilan qiymetden xercler cixilaraq gelire elave edir ve mehsul sayini azaldir.
+
+//QEYD:Butun fieldler encapsule edilmelidir. Encapsule shertlerini ozunuz mentiqle qurun
